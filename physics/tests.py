8b134c136ac73988ca1ae70c7c8b2f661d3fe070
Kvík <martin.balaz@trojsten.sk>
2018-11-20 17:04:04 +0000

    Fixed tests

--- physics/tests.py
+++ physics/tests.py
@@ -44,7 +44,7 @@ class CaseRadiometry(unittest.TestCase):
         self.assertEqual(radiometry.fluxDensity(4 * math.pi, 1), 1)
 
     def testApparentMagnitudeSun(self):
-        self.assertAlmostEqual(radiometry.apparentMagnitude(1370), constants.apparentMagnitudeSun, delta = 0.01)
+        self.assertAlmostEqual(radiometry.apparentMagnitude(1361), constants.apparentMagnitudeSun, delta = 0.01)
 
 class CaseMeteor(unittest.TestCase):
     def setUp(self):
@@ -57,18 +57,19 @@ class CaseMeteor(unittest.TestCase):
             longitude   = 18,
             elevation   = 531,
         )
+        self.position = coord.Vector3D.fromGeodetic(48, 17, 120000)
         self.meteor = meteor.Meteor(
             mass        = 1,
             density     = 800,
-            position    = coord.Vector3D.fromGeodetic(48, 17, 120000),
-            velocity    = -position / position.norm() * 50000,
+            position    = self.position,
+            velocity    = -self.position / self.position.norm() * 50000,
             timestamp   = datetime.datetime.now(),
         )
         self.meteor.flyRK4(10, 10)
         self.sighting = sighting.PointSighting(sighting.Sighting(self.observer, self.meteor))
 
     def testSimpleMeteor(self):
-        
+        pass
 
 class CaseVector3D(unittest.TestCase):
     def setUp(self):
